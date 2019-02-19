5619f2f483b9366b48a32f8e541eda6add3ea31b
4hlberg <philip.ahlberg@sesam.io>
2019-02-19 14:17:44 +0100

    only get method for testing for now

--- service/currenttime.py
+++ service/currenttime.py
@@ -2,7 +2,6 @@ from flask import Flask, request, Response
 import os
 import requests
 import logging
-import sys
 import json
 import dotdictify
 
@@ -18,33 +17,51 @@ logger.addHandler(stdout_handler)
 logger.setLevel(logging.DEBUG)
 
 
+class DataAccess:
+
+#main get function, will probably run most via path:path
+
+    def __get_all_paged_entities(self, path, args):
+        logger.info("Fetching data from url: %s", path)
+        url = os.environ.get("base_url") + path
+        req = requests.get(url, headers={"Accept":"Application/json", "Authorization": "Basic " + os.environ.get('basic_token')})
+
+        if req.status_code != 200:
+            logger.error("Unexpected response status code: %d with response text %s" % (req.status_code, req.text))
+            raise AssertionError ("Unexpected response status code: %d with response text %s"%(req.status_code, req.text))
+        res = dotdictify.dotdictify(json.loads(req.text))
+        for entity in res:
+
+            yield(entity)
+        logger.info('Returning entities')
+
+
+    def get_entities(self,path, args):
+        print("getting all entities")
+        return self.__get_all_paged_entities(path, args)
+
+data_access_layer = DataAccess()
+
+
+def stream_json(entities):
+    first = True
+    yield '['
+    for i, row in enumerate(entities):
+        if not first:
+            yield ','
+        else:
+            first = False
+        yield json.dumps(row)
+    yield ']'
+
 @app.route("/<path:path>", methods=["GET", "POST"])
-def employee_methods(path):
-    if request.method == "POST":
-        post_url = os.environ.get('base_url') + path
-        logger.info(request.get_json())
-        entities = request.get_json()
-        headers = json.loads(os.environ.get('headers').replace("'", "\""))
-        logger.info('Sending entites to %s', path)
-        response = requests.post(post_url, data=entities, headers=headers)
-        if response.status_code is not 200:
-            logger.error("Got error code: " + str(response.status_code) + "with text: " + response.text)
-            return Response(response.text, status=response.status_code, mimetype='application/json')
-        logger.info("Prosessed " + str(len(entities)) + " entities")
-        return Response(response.text, status=response.status_code, mimetype='application/json')
+def get(path):
+    entities = data_access_layer.get_entities(path, args=request.args)
 
-    elif request.method == "GET":
-        logger.info("wrong method")
-    else:
-        sys.exit()
-    return Response(entities)
-
-
-    # Will be taken in to use later, next iteration after test
-    # return Response(
-    #     stream_json(entities),
-    #     mimetype='application/json'
-    # ){AttributeError}'str' object has no attribute 'items'
+    return Response(
+        stream_json(entities),
+        mimetype='application/json'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('port',5000))
